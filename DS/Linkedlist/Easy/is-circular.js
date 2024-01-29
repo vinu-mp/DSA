65d9d3141379f4c4169b7c3577f308c1bbbed402
vinu-mp <mp.prasadvinu@gmail.com>
2024-01-29 08:15:27 +0530

    Linked list solutions

--- DS/Linkedlist/Easy/is-circular.js
+++ DS/Linkedlist/Easy/is-circular.js
@@ -26,18 +26,30 @@
 // Explanation: There is no cycle in the linked list.
 
 var hasCycle = function(head) {
-  let slow = head
-  let fast = head
-  console.log(head.next);
-  while (fast && fast.next && fast.next.next) {
-      slow = slow.next
-      fast = fast.next.next
-      if (slow === fast) {
-          return true
-      }
+  // let slow = head
+  // let fast = head
+  // console.log(head.next);
+  // while (fast && fast.next && fast.next.next) {
+  //     slow = slow.next
+  //     fast = fast.next.next
+  //     if (slow === fast) {
+  //         return true
+  //     }
+  // }
+
+  // return false
+
+  // hash table
+  let current = head, set = new Set();
+  while(current) {
+    if(set.has(current)) {
+      return true;
+    }
+    set.add(current, true);
+    current = current.next
   }
 
-  return false
+  return false;
 };
 
 console.log(hasCycle([3,2,0,-4]));
